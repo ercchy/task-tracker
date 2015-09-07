14f736e665b339ca436a4612c8315890c35b7e29
ercchy <erika@serox.si>
2015-09-07 18:45:28 +0200

    PEP8 fixes for tracker/site/urls.py file

--- tracker/site/urls.py
+++ tracker/site/urls.py
@@ -11,27 +11,27 @@ from .views import (
 	update_project_view
 )
 
-
 urlpatterns = patterns(
-    '',
-    url(r'^projects/$', project_list_view, name='project-list'),
-    url(r'^projects/create/$', create_project_view, name='project-create'),
-    url(
-    	r'^projects/(?P<project_id>\d+)/tickets/create$',
-    	create_ticket_view,
-    	name='ticket-create'
+	'',
+	url(r'^projects/$', project_list_view, name='project-list'),
+	url(r'^projects/create/$', create_project_view, name='project-create'),
+	url(
+		r'^projects/(?P<project_id>\d+)/tickets/create$',
+		create_ticket_view,
+		name='ticket-create'
 	),
-    url(
-    	r'^projects/(?P<project_id>\d+)/tickets/(?P<ticket_id>\d+)/edit$',
-    	update_ticket_view,
-    	name='ticket-update'
+	url(
+		r'^projects/(?P<project_id>\d+)/tickets/(?P<ticket_id>\d+)/edit$',
+		update_ticket_view,
+		name='ticket-update'
 	),
-    url(
-    	r'^projects/(?P<project_id>\d+)/edit/$',
-    	update_project_view,
-    	name='project-update'
+	url(
+		r'^projects/(?P<project_id>\d+)/edit/$',
+		update_project_view,
+		name='project-update'
 	),
-    url(
-    	r'^projects/(?P<project_id>\d+)/$', project_view, name='project-detail'),
-    url(r'^$', my_tickets_view, name='my-tickets'),
+	url(
+		r'^projects/(?P<project_id>\d+)/$', project_view,
+		name='project-detail'),
+	url(r'^$', my_tickets_view, name='my-tickets'),
 )
