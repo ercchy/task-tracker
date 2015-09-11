2994a2b965cad5ea44c67ee23da2f84cfe1127ef
ercchy <erika@serox.si>
2015-09-11 17:55:35 +0200

    Add missing files

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var sass = require('gulp-sass');
 var concat = require('gulp-concat');
 var livereload = require('gulp-livereload');
 
+var baseCodeDir = 'tracker/site'
 var baseTemplateDir = 'tracker/templates';
 var baseSrcDir = 'tracker/static-dev';
 var baseDestDir = 'tracker/static';
@@ -43,7 +44,12 @@ gulp.task('copy-js', function () {
 
 gulp.task('html', function () {
 	gulp.src(baseTemplateDir + '/**/*.html')
-		.pipe(changed('.'))
+		//.pipe(changed('.'))
+		.pipe(livereload());
+});
+
+gulp.task('code', function () {
+	gulp.src(baseCodeDir + '/*.py')
 		.pipe(livereload());
 });
 
@@ -54,7 +60,8 @@ gulp.task('build-styles', ['sass', 'copy-foundation-fonts'])
 gulp.task('watch', function() {
   livereload.listen();
   gulp.watch(baseSrcDir + '/scss/*.scss', ['sass']);
-  gulp.watch(baseTemplateDir + '/*.html', ['html']);
+  gulp.watch(baseTemplateDir + '/**/*.html', ['html']);
+  gulp.watch(baseCodeDir + '/*.py', ['code']);
 });
 
 gulp.task('build', ['build-styles', 'copy-styles', 'copy-js', 'concat-js'])
